5c469ca3e68d322b6c211f0b4e0eb591fe43f11b
tjstetson <82979469+tjstetson@users.noreply.github.com>
2021-08-20 16:27:20 -0700

    Final version
    Cleaned up form, export to excel and data pushed to google sheets from form.

--- flask_app/CGReport/routes.py
+++ flask_app/CGReport/routes.py
@@ -1,106 +1,82 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,flash
 from CGReport import app,forms,db,session
-from CGReport.gis_helpers import get_data_feature_layer
 import pandas as pd
 from werkzeug.utils import secure_filename
 import os
+from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 @app.route("/")
 def index():
     return render_template('index.html',title="Report Generation-Map")
 
-@app.route("/parcels", methods=['GET','POST'])
-def parcels():
-    return render_template("parcels.html",title='Parcel Search')
 
-@app.route("/apn_search",methods=['GET','POST'])
-def apnSearch():
-    apnForm = forms.ApnForm()
-    if apnForm.validate_on_submit():
-        print("apn")
-        econLyer = get_data_feature_layer('econ')
-        
-        wc=f"APN ='{apnForm.APN.data}'"
-        parcels = econLyer.query(where=wc,out_fields="APN,Acreage,NMTC,FTZ,OZ,Zoning",as_df=True)
-        del parcels['SHAPE']
-        del parcels['FID'] 
-        parcelsDict = parcels.to_dict()
-        session['parcelresults'] = parcelsDict
-        session['psmessage'] = 'No Results Found'
-        return redirect(url_for('parcelresults'))       
-    else:
-        apnForm = forms.ApnForm()
-        return render_template('apn_search.html',apnForm=apnForm)
-
-@app.route("/zone_search",methods=['GET','POST'])
-def zoneSearch():
-    zoneForm = forms.ZoneForm()
-
-    if zoneForm.validate_on_submit():
-        econLyer = get_data_feature_layer('econ')
-        print('zone')
-        wc=f"ZONING ='{zoneForm.ZONE.data}'"
-        parcels = econLyer.query(where= wc,out_fields="APN,Acreage,NMTC,FTZ,OZ,Zoning",as_df=True)
-        del parcels['SHAPE']
-        del parcels['FID']
-        parcelsDict = parcels.to_dict()
-        session['parcelresults'] = parcelsDict
-        return redirect(url_for('parcelresults'))
-    else:
-        zoneForm = forms.ZoneForm()
-        return render_template('zone_search.html',zoneForm=zoneForm)
-
-@app.route("/parcel_search",methods=['GET','POST'])
-def parcelSearch():
-    parcelForm = forms.ParcelForm()
-    if parcelForm.is_submitted():
-        parcelForm.validate()
-        econLyer = get_data_feature_layer('econ')
-        
-        #create where clause for query
-        wc = f"ACREAGE BETWEEN {parcelForm.aMin.data} AND {parcelForm.aMax.data} AND NMTC = '{parcelForm.NMTC.data}' AND OZ = '{parcelForm.OZ.data}' AND ZONING = '{parcelForm.ZONE.data}'"
-        
-        parcels = econLyer.query(where=wc,out_fields="APN,Acreage,NMTC,FTZ,OZ,Zoning",as_df=True)
-        del parcels['SHAPE']
-        del parcels['FID']
-        if parcels.shape[0] == 0:
-            session['psmessage'] = 'No results found.'
-            return redirect(url_for('parcelSearch'))
-        else:
-            parcelsDict = parcels.to_dict()
-            session['parcelresults'] = parcelsDict
-            return redirect(url_for('parcelresults'))
-    
-    parcelForm = forms.ParcelForm()
-    return render_template('parcel_search.html',parcelForm=parcelForm)
-
-@app.route("/parcelresults",methods=['GET','POST'])
-def parcelresults():
-    # get parcelsDict from session convert to dataframe then to html
-    if 'parcelresults' in session:
-        data = session['parcelresults']
-        parcelresults = pd.DataFrame(data)
-        return render_template("parcelresults.html",parcelresults=parcelresults.to_html(index=False), title="Results")
-
-    message = 'No Data Found'  
-    return render_template('parcelresults.html',title="Parcel Search Results",message = message)
 
 @app.route("/report",methods=['GET','POST'])
 def report():
-    # save csv or xlsx file to file system
-    form = forms.Upload()
-    if form.is_submitted():
-        form.validate()
-        filename = secure_filename(form.file.data.filename)
-        form.file.data.save(os.path.join(
-            'CGReport', 'uploads', filename
-        ))    
-        return redirect(url_for('reportsupinfo'))
+    
 
-    form = forms.Upload()
-    return render_template('report.html',form=form)
+    ReportForm = forms.ReportForm()
+    if ReportForm.is_submitted():
+        ReportForm.validate()
 
-@app.route('/report/supinfo',methods=['GET','POST'])
-def reportsupinfo():
+        reportDict = {}
+        for field in ReportForm:
+            if field.label.text == "Generate Report" or field.label.text == "CSRF Token":
+                continue
+            reportDict[field.label.text]=field.data
+        
+        reportdf= pd.DataFrame.from_dict(reportDict,orient='index')
+        
+        now = datetime.now()
+        dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
+        reportName = f"site{ReportForm.siteNum.data}report{dt_string}.xlsx"
+        path = os.path.dirname(os.path.abspath(__file__))
+        
+        
+        reportdf.to_excel(os.path.join(path,'static','reports',reportName))
+        
+        ## push data to google sheet
+        ## set scope and credentials to connect to sheets and drive api
+        scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+        credfile = os.path.join(path,'cgcreds.json')
+        creds = ServiceAccountCredentials.from_json_keyfile_name(credfile, scope)
+        client = gspread.authorize(creds)
+        sheet = client.open("Casa Grande Site Information").sheet1
 
-    return render_template("supinfo.html")
+        ## get values from dictionary into list
+        col_vals = list(reportDict.values())
+        for val in col_vals:
+            if val == None:
+                val = " "
+                
+        col_count = sheet.col_count
+        last_col = len(sheet.row_values(1))
+        ## check if there is an empty column to add to or create one if there isn't
+        if col_count > last_col:
+            sheet.insert_cols([col_vals],col= last_col+1,value_input_option='RAW')
+        else:
+            sheet.add_cols(1)
+            sheet.insert_cols([col_vals],col= last_col+1,value_input_option='RAW')
+
+
+        return redirect(url_for('reportsuccess'))
+
+    ReportForm = forms.ReportForm()
+    return render_template('report.html',ReportForm=ReportForm)
+
+@app.route('/reportsuccess',methods=['GET','POST'])
+def reportsuccess():
+    path = os.path.dirname(os.path.abspath(__file__))
+    files= os.listdir(os.path.join(path,'static','reports'))
+    message="Report Success"
+    return render_template('reportsuccess.html',message=message,files=files)
+
+@app.route('/downloads',methods=['GET','POST'])
+def downloads():
+    path = os.path.dirname(os.path.abspath(__file__))
+    files= os.listdir(os.path.join(path,'static','reports'))
+    
+    return render_template('downloads.html',files=files)
+
